9355cc6d6732902728f66ecc99ee60e086385b33
TJMFL <161985881+TJMFL@users.noreply.github.com>
2025-03-24 20:04:32 -0500

    Update Booking.tsx

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -168,7 +168,7 @@ const Booking = () => {
         </div>
 
         {/* Content */}
-        <div className="container relative z-20 px-4 md:px-8 pt-40 md:pt-56 lg:pt-72">
+        <div className="container relative z-20 px-2 md:px-4 pt-40 md:pt-56 lg:pt-72">
           <div className="max-w-3xl">
             <p className="text-red-500 uppercase tracking-widest text-sm mb-2 font-medium">
               Book Our Services
